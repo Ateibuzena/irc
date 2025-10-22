28bb8f56ae0889657c0fc1787c4e250715b7af5a
Ana Zubieta Pena <azubieta@c3r8s3.42malaga.com>
2025-10-22 14:34:33 +0200

    [Logic] Modificando comando QUit

--- srcs/logic/serverlogic/ServerLogicHandlers.cpp
+++ srcs/logic/serverlogic/ServerLogicHandlers.cpp
@@ -1,5 +1,4 @@
-#include "ServerLogic.hpp"
-
+#include "../../../includes/logic/ServerLogic.hpp"
 
 /*----------------------------------HELPERS-----------------------------------*/
 
@@ -10,7 +9,7 @@ void    ServerLogic::handlePASS(Client* client, const Command& cmd)
 {
     const std::string& password = cmd.params[0];
 
-    if (client->getRegistered() == true)
+    if (client->isRegistered())
         throw (ERR_ALREADYREGISTERED);
 
     if (password != _serverPassword)
@@ -25,26 +24,125 @@ void    ServerLogic::handlePASS(Client* client, const Command& cmd)
         client->setRegistered(true);
 }
 
+// Manejar el comando QUIT (desconectar cliente)
 void    ServerLogic::handleQUIT(Client* client, const Command& cmd)
 {
-    std::string quitMessage = "Client disconnected";
+    std::string msg = "Client disconnected";
     if (!cmd.params.empty())
-        quitMessage = cmd.params[0];
+    {
+        msg.clear();
+
+        // Construimos el mensaje completo concatenando todos los parámetros a partir del 1
+        /*msg = cmd.params[0];*/
+        size_t i = 0;
+        while (i < cmd.params.size())
+        {
+            if (i > 1)
+                msg += " ";
+            msg += cmd.params[i];
+            i++;
+        }
+        if (!msg.empty() && msg[0] == ':')
+            msg = msg.substr(1);
+    }
+
+    std::string fullMsg = buildMessage(client->getNickname(), "QUIT", "", msg);
 
     // Enviar mensaje de QUIT a todos los canales donde esté el cliente
-    const std::set<Channel*>& channels = client->getChannels();
-    std::set<Channel*>::const_iterator it = channels.begin();
-    while (it != channels.end())
+    const std::set<std::string>& channelsNames = client->getChannels();
+    std::set<std::string>::const_iterator it = channelsNames.begin();
+    while (it != channelsNames.end())
     {
-        (*it)->broadcast(quitMessage, client);
-        (*it)->removeClient(client);
+        std::map<std::string, Channel*>::iterator chanIt = _channels.find(*it);
+        if (chanIt != _channels.end())
+        {
+            Channel* channel = chanIt->second;
+            channel->broadcast(fullMsg, client);
+            channel->removeClient(client);
+        }
         ++it;
     }
 
     // Eliminar cliente del servidor
     serverRemoveClient(client->getFd());
 
+    std::cout << RED << "❌ Client quit: " << client->getNickname() << RESET << std::endl;
+
     delete (client);
+}
+
+/*Channel Operations*/
+
+void    ServerLogic::handleJOIN(Client* client, const Command& cmd)
+{
+    const std::string& channelName = cmd.params[0];
+
+    /*if (channel->isInviteOnly() && !channel->isInvited(client) && !channel->isOperator(client))
+        throw (ERR_INVITEONLYCHAN);*/
+    if (!client->isRegistered())
+        throw (ERR_NOTREGISTERED);
+    try
+    {
+        Channel* channel = createChannel(channelName, client);
+
+        if (channel->getClients().size() >= channel->getMaxClients())
+            throw (ERR_CHANNELISFULL);
+
+        channel->addClient(client);
+        client->joinChannel(channel);
+    }
+    catch (int  error)
+    {
+        throw (error);
+    }
+}
+
+void    ServerLogic::handleINVITE(Client* client, const Command& cmd)
+{
+    const std::string& channelName = cmd.params[0];
+    const std::string& nickname = cmd.params[1];
+
+    if (!client->isRegistered())
+        throw (ERR_NOTREGISTERED);
+
+    // Primero buscamos el canal
+    std::map<std::string, Channel*>::iterator chanIt = _channels.find(channelName);
+    if (chanIt == _channels.end())
+        throw (ERR_NOSUCHCHANNEL);
+    Channel* channel = chanIt->second;
+
+    // Si el canal es invite-only, solo los operadores pueden invitar
+    if (channel->isInviteOnly() && !channel->isOperator(client))
+        throw (ERR_CHANOPRIVSNEEDED);
+
+    if (channel->getClients().size() >= channel->getMaxClients())
+        throw (ERR_CHANNELISFULL);
+
+    // Luego buscamos el cliente a invitar
+    std::map<std::string, Client*>::iterator nickIt = _nicknames.find(nickname);
+    if (nickIt == _nicknames.end())
+        throw (ERR_NOSUCHNICK);
+    Client* invitedClient = nickIt->second;
+
+    // Invitamos al cliente al canal
+    channel->inviteClient(invitedClient);
+
+    // Enviamos el mensaje de invitación
+    std::string inviteMsg = buildMessage(client->getNickname(), "INVITE", invitedClient->getNickname(), channelName);
+    channel->broadcast(inviteMsg, client);
+}
+
+void    ServerLogic::handleKICK(Client* client, const std::string& channelName, const std::string& nickname, const std::string& reason)
+{
+
+}
+
+
+/*Server Queries and Information*/
+
+void    ServerLogic::handleMODE(Client* client, const Command& cmd)
+{
+
 }
 
 /*User Registration*/
@@ -80,7 +178,7 @@ void    ServerLogic::handleUSER(Client* client, const Command& cmd)
 {
     const std::string username = cmd.params[0];
 
-    if (client->getRegistered() == true)
+    if (client->isRegistered())
         throw (ERR_ALREADYREGISTERED);
 
     client->setUsername(username);
@@ -92,36 +190,10 @@ void    ServerLogic::handleUSER(Client* client, const Command& cmd)
         client->setRegistered(true);
 }
 
-/*Channel Operations*/
-
-void    ServerLogic::handleJOIN(Client* client, const Command& cmd)
-{
-    const std::string& channelName = cmd.params[0];
-
-    /*if (channel->isInviteOnly() && !channel->isInvited(client) && !channel->isOperator(client))
-        throw (ERR_INVITEONLYCHAN);*/
-    if (!client->getRegistered())
-        throw (ERR_NOTREGISTERED);
-    try
-    {
-        Channel* channel = createChannel(channelName, client);
-
-        if (channel->getClients().size() >= channel->getMaxClients())
-            throw (ERR_CHANNELISFULL);
-
-        channel->addClient(client);
-        client->joinChannel(channel);
-    }
-    catch (int  error)
-    {
-        throw (error);
-    }
-}
-
 // Salir de un canal
 void    ServerLogic::handlePART(Client* client, const Command& cmd)
 {
-    if (!client->getRegistered())
+    if (!client->isRegistered())
         throw (ERR_NOTREGISTERED);
 
     // Si el último parámetro empieza por ':', es mensaje
@@ -172,7 +244,7 @@ void    ServerLogic::handleTOPIC(Client* client, const Command& cmd)
 {
     const std::string& channelName = cmd.params[0];
 
-    if (!client->getRegistered())
+    if (!client->isRegistered())
         throw (ERR_NOTREGISTERED);
     
     // Primero buscamos el canal
@@ -231,59 +303,11 @@ void    ServerLogic::handleTOPIC(Client* client, const Command& cmd)
     channel->broadcast(msg, NULL);
 }
 
-void    ServerLogic::handleINVITE(Client* client, const Command& cmd)
-{
-    const std::string& channelName = cmd.params[0];
-    const std::string& nickname = cmd.params[1];
-
-    if (!client->getRegistered())
-        throw (ERR_NOTREGISTERED);
-
-    // Primero buscamos el canal
-    std::map<std::string, Channel*>::iterator chanIt = _channels.find(channelName);
-    if (chanIt == _channels.end())
-        throw (ERR_NOSUCHCHANNEL);
-    Channel* channel = chanIt->second;
-
-    // Si el canal es invite-only, solo los operadores pueden invitar
-    if (channel->isInviteOnly() && !channel->isOperator(client))
-        throw (ERR_CHANOPRIVSNEEDED);
-
-    if (channel->getClients().size() >= channel->getMaxClients())
-        throw (ERR_CHANNELISFULL);
-
-    // Luego buscamos el cliente a invitar
-    std::map<std::string, Client*>::iterator nickIt = _nicknames.find(nickname);
-    if (nickIt == _nicknames.end())
-        throw (ERR_NOSUCHNICK);
-    Client* invitedClient = nickIt->second;
-
-    // Invitamos al cliente al canal
-    channel->inviteClient(invitedClient);
-
-    // Enviamos el mensaje de invitación
-    std::string inviteMsg = buildMessage(client->getNickname(), "INVITE", invitedClient->getNickname(), channelName);
-    channel->broadcast(inviteMsg, client);
-}
-
-void    ServerLogic::handleKICK(Client* client, const std::string& channelName, const std::string& nickname, const std::string& reason)
-{
-
-}
-
-
-/*Server Queries and Information*/
-
-void    ServerLogic::handleMODE(Client* client, const Command& cmd)
-{
-
-}
-
 /*Sending Messages*/
 
 void    ServerLogic::handleNOTICE(Client* client, const Command& cmd)
 {
-    if (client->getRegistered() == false)
+    if (!client->isRegistered())
         throw (ERR_NOTREGISTERED);
 
     const std::string& target = cmd.params[0];  
@@ -327,7 +351,7 @@ void    ServerLogic::handleNOTICE(Client* client, const Command& cmd)
 // Manejar el comando PRIVMSG (enviar mensaje privado)
 void    ServerLogic::handlePRIVMSG(Client* client, const Command& cmd)
 {
-    if (client->getRegistered() == false)
+    if (!client->isRegistered())
         throw (ERR_NOTREGISTERED);
     
     const std::string& target = cmd.params[0];
