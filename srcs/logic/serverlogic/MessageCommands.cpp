7ff3a42eded729ce1ed40c2dbd18aec5f88ebe46
Ana Zubieta Pena <azubieta@c2r9s3.42malaga.com>
2025-10-23 16:42:44 +0200

    [Logic] terminando funcion privmsg

--- srcs/logic/serverlogic/MessageCommands.cpp
+++ srcs/logic/serverlogic/MessageCommands.cpp
@@ -38,18 +38,29 @@ void    ServerLogic::handleNOTICE(Client* client, const Command& cmd)
 // Manejar el comando PRIVMSG (enviar mensaje privado)
 void    ServerLogic::handlePRIVMSG(Client* client, const Command& cmd)
 {
+    // Verificamos que el cliente esté registrado
     if (!client->isRegistered())
         throw (ERR_NOTREGISTERED);
 
     std::vector<std::string> receivers = str_to_vector(cmd.params[0], ',');
+
+    // Verificamos si hay mensaje
     std::string msg = cmd.params[1];
+    if (msg.empty())
+        throw (ERR_NOTEXTTOSEND);
+
     size_t i = 0;
     while (i < receivers.size())
     {
+        // Obtenemos el destinatario actual
         const std::string& target = receivers[i];
+        if (target.empty())
+            throw (ERR_NORECIPIENT);
+
+        // Construimos el mensaje completo
         std::string fullMsg = buildMessage(client->getNickname(), "PRIVMSG", target, msg);
 
-        // Buscamos si es un cliente por nickname
+        // Buscamos si es un cliente
         std::map<std::string, Client*>::iterator nickIt = _serverNicknames.find(target);
         if (nickIt != _serverNicknames.end())
         {
