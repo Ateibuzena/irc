94e9893593d099693b1f4107fda4302e6c61a42c
Ana Zubieta Pena <azubieta@c2r8s4.42malaga.com>
2025-10-29 03:09:53 +0100

    norminette

--- includes/parser/Parser.hpp
+++ includes/parser/Parser.hpp
@@ -7,23 +7,28 @@
 #include <map>
 #include "../utils/Command.hpp"
 
-class Parser {
-    public:
-        Command parse(const std::string &rawMessage);
-
+class Parser
+{
     private:
-        static std::map<std::string, int> commands;
+    
+        static std::map<std::string, int>   commands;
 
-        bool ft_isvalidusername(const std::string& name);
-        void ft_params(const std::string &rawMessage, Command &toret, unsigned long i, unsigned long size);
-        void ft_lastparam(const std::string &rawMessage, Command &toret, unsigned long i, unsigned long size);
-        void ft_parsecommand(Command toret);
-        bool ft_checkchannel(std::string str);
-        bool ft_checkkeys(std::string str);
-        bool ft_checkchanneluser(std::string str);
-        bool ft_checkskey(std::string str);
-        bool ft_checknumber(std::string str);
-        bool ft_checksinglechannel(std::string str);
+        bool                                ft_isvalidusername(const std::string& name);
+        void                                ft_params(const std::string &rawMessage, Command &toret, unsigned long i, unsigned long size);
+        void                                ft_lastparam(const std::string &rawMessage, Command &toret, unsigned long i, unsigned long size);
+        void                                ft_parsecommand(Command toret);
+        bool                                ft_checkchannel(std::string str);
+        bool                                ft_checkkeys(std::string str);
+        bool                                ft_checkchanneluser(std::string str);
+        bool                                ft_checkskey(std::string str);
+        bool                                ft_checknumber(std::string str);
+        bool                                ft_checksinglechannel(std::string str);
+
+    public:
+
+        void                                initCommands();
+        Command                             parse(const std::string &rawMessage);
+
 };
 
 #endif
