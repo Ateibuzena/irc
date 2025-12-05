23098388b2137d6ec1250ffe2fd37e7dbc71f285
juan-ant <juanpinpon51@gmail.com>
2025-12-05 15:21:02 +0100

    fixed port

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -8,11 +8,16 @@ int main(int argc, char* argv[])
         return (1);
     }
 
+    if (std::atoi(argv[1]) < 1024 || std::atoi(argv[1]) > 65535)
+    { 
+        std::cerr << "Invalid port" << std::endl;
+        return (1);
+    }
+
     initErrorMessages();
     initReplayMessages();
 
     Server srv(atoi(argv[1]), argv[2]);
-
     int status = srv.run();
     return (status);
 }
